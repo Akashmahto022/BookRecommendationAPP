34895fbf4ead7f70cd55ce4643cfdd13280153f1
technicalsupport3 <technicalsupport3@aafm.co.in>
2025-03-19 13:14:42 +0530

    created register route

--- backend/src/models/User.model.js
+++ backend/src/models/User.model.js
@@ -22,5 +22,19 @@ const userSchema = new mongoose.Schema({
     },
 }, { timestamps: true });
 
+// hash password before saving user to db
+
+userSchema.pre("save", async function (next) {
+
+    if (!this.isModified("password")) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+
+    next();
+})
+
 export const User = mongoose.model("User", userSchema);
 
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,10 +1,42 @@
 import express from "express";
+import { User } from "../models/User.model";
 
 const router = express.Router()
 
 router.post("/register", async (req, res) => {
     try {
-        
+        const { name, email, password } = req.body;
+
+        if (!username || !email || !password) {
+            return res.status(400).json({ error: "All fields are required" })
+        }
+
+
+        if (password.length < 6) {
+            return res.status(400).json({ error: "Password must be atleast 6 characters" })
+        }
+
+        if (username.length < 3) {
+            return res.status(400).json({ error: "Username must be atleast 3 characters" })
+        }
+
+        // check if user already exists
+
+        const existingUser = await User.findOne({$or:[{email},{username}]})
+        if (existingUser) {
+            return res.status(400).json({ error: "User already exists with this email and username" })
+        }
+
+        const profileImage = `https://api.dicebear.com/9.x/avataaars/svg?seed=${userName}`;
+
+        const user = new User({
+            username,
+            email,
+            password,
+            profileImage
+        })
+
+
     } catch (error) {
 
     }
