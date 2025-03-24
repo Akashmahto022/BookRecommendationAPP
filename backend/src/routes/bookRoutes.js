8bac076fda5a079754f352f42473eaf4cb12e5a3
technicalsupport3 <technicalsupport3@aafm.co.in>
2025-03-24 13:17:24 +0530

    create a api for deleting the book

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import cloudinary from "../lib/cloudinary"
+import cloudinary from "../lib/cloudinary.js"
 import { Book } from "../models/Book.model.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
@@ -68,6 +68,40 @@ router.get("/get-books", protectRoute, async (req, res) => {
 
     } catch (error) {
         console.log("error while getting books", error.message)
+        res.status(500).json({ message: error.message })
+    }
+})
+
+router.delete("/delete-books/:id", protectRoute, async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id)
+
+        if (!book) {
+            return res.status(404).json({ message: "Book not found" })
+        }
+
+        // check the user is the owner of the book
+        if (book.user.toString() != req.user._id.toString()) {
+            return res.status(404).json({ message: "user not authorized to delete this book" })
+        }
+
+        // delete the image from cloudinary
+        if (book.image && book.image.includes("cloudinary")) {
+            try {
+                const publicId = book.image.split("/").pop().split(".")[0]
+                await cloudinary.uploader.destroy(publicId);
+            } catch (deleteError) {
+                console.log("error while delete image from cloudinary", deleteError)
+
+            }
+        }
+
+        await book.deleteOne()
+
+        res.json({ message: "book delete successfully" })
+
+    } catch (error) {
+        console.log("error while deleting books", error.message)
         res.status(500).json({ message: error.message })
     }
 })
