385f921726cdaac0d9107921aadead0d86452596
technicalsupport3 <technicalsupport3@aafm.co.in>
2025-03-25 18:08:19 +0530

    full setup for the mobile app

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,15 +1,26 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Hello This App is building by Akash Mahto</Text>
+      <Text style={styles.title}>Hello This App is building by Akash Mahto</Text>
+      <Text style={styles.title}>Hi There</Text>
+
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container : {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    color: "red",
+    backgroundColor: "black",
+    padding: "auto"
+  }
+})
